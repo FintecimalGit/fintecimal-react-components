30bddf404066fac7c9984cd48667d9a44cff892c
juan <juan@MacBook-Pro-4.local>
2019-05-28 18:04:36 -0500

    compile to ecma 5

--- dist/Pagination.js
+++ dist/Pagination.js
@@ -11,47 +11,86 @@ require("../styles/Pagination.css");
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
 
-const imageDisable = 'https://s3.us-east-2.amazonaws.com/fintecimal/fintecimal-img/arrow-left-menu-disable.svg';
-const imageActive = 'https://s3.us-east-2.amazonaws.com/fintecimal/fintecimal-img/arrow-left.svg';
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
-class Pagination extends _react.Component {
-  checkNumber(sum) {
-    const _this$props = this.props,
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+var imageDisable = 'https://s3.us-east-2.amazonaws.com/fintecimal/fintecimal-img/arrow-left-menu-disable.svg';
+var imageActive = 'https://s3.us-east-2.amazonaws.com/fintecimal/fintecimal-img/arrow-left.svg';
+
+var Pagination =
+/*#__PURE__*/
+function (_Component) {
+  _inherits(Pagination, _Component);
+
+  function Pagination() {
+    _classCallCheck(this, Pagination);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(Pagination).apply(this, arguments));
+  }
+
+  _createClass(Pagination, [{
+    key: "checkNumber",
+    value: function checkNumber(sum) {
+      var _this$props = this.props,
           actualPage = _this$props.actualPage,
           totalPage = _this$props.totalPage;
-    const newCount = actualPage + sum;
+      var newCount = actualPage + sum;
 
-    if (!(newCount < 1 || newCount > totalPage)) {
-      this.props.onChangePagination(newCount);
+      if (!(newCount < 1 || newCount > totalPage)) {
+        this.props.onChangePagination(newCount);
+      }
     }
-  }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this = this;
 
-  render() {
-    const _this$props2 = this.props,
+      var _this$props2 = this.props,
           totalPage = _this$props2.totalPage,
           actualPage = _this$props2.actualPage;
-    return _react.default.createElement("div", {
-      className: "fnt-pagination"
-    }, _react.default.createElement("div", {
-      className: "ftn-pagination-arrow-left",
-      onClick: () => this.checkNumber(-1)
-    }, _react.default.createElement("img", {
-      alt: "",
-      src: actualPage === 1 ? imageDisable : imageActive
-    })), _react.default.createElement("div", {
-      className: "ftn-pagination-body"
-    }, _react.default.createElement("span", {
-      className: "ftn-pagination-text"
-    }, "P\xE1gina ", actualPage, " de ", totalPage)), _react.default.createElement("div", {
-      className: "ftn-pagination-arrow-right",
-      onClick: () => this.checkNumber(1)
-    }, _react.default.createElement("img", {
-      alt: "",
-      src: actualPage >= totalPage ? imageDisable : imageActive
-    })));
-  }
+      return _react.default.createElement("div", {
+        className: "fnt-pagination"
+      }, _react.default.createElement("div", {
+        className: "ftn-pagination-arrow-left",
+        onClick: function onClick() {
+          return _this.checkNumber(-1);
+        }
+      }, _react.default.createElement("img", {
+        alt: "",
+        src: actualPage === 1 ? imageDisable : imageActive
+      })), _react.default.createElement("div", {
+        className: "ftn-pagination-body"
+      }, _react.default.createElement("span", {
+        className: "ftn-pagination-text"
+      }, "P\xE1gina ", actualPage, " de ", totalPage)), _react.default.createElement("div", {
+        className: "ftn-pagination-arrow-right",
+        onClick: function onClick() {
+          return _this.checkNumber(1);
+        }
+      }, _react.default.createElement("img", {
+        alt: "",
+        src: actualPage >= totalPage ? imageDisable : imageActive
+      })));
+    }
+  }]);
 
-}
+  return Pagination;
+}(_react.Component);
 
 ;
 Pagination.defaultProps = {
