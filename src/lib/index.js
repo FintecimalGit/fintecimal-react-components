937c4362ec7caadc29df3ec108eba10f6a9de1ed
juan <juan@MacBook-Pro-4.local>
2019-05-30 16:40:35 -0500

    add dropdown

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,10 +2,12 @@ import Input from './Input';
 import Button from './Button';
 import Pagination from './Pagination';
 import Tables from './Tables';
+import Dropdown from './Dropdown';
 
 export default {
 	Input, 
 	Button,
 	Pagination,
-	Tables
+	Tables,
+	Dropdown
 }
