dc730f848e48a4528e6bd5365e4bc39fa53709e6
Benjamin Gonzalez <benjamin@fintecimal.com>
2019-12-12 18:17:13 -0600

    Move key maps to .env

--- src/lib/nodes/SignatureMap/geoCoding.js
+++ src/lib/nodes/SignatureMap/geoCoding.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 const URL = ' https://maps.googleapis.com/maps/api';
 
+const { MAP_KEY } = process.env;
+
 const getGeocoding = async ({ lat, lng }) => {
   try {
-    const { data } = await axios.get(
-      `${URL}/geocode/json?latlng=${lat},${lng}&key=AIzaSyA3C5AilsVhn6xNYDJ73B0eKJwBNFe2RNc`
-    );
+    const { data } = await axios.get(`${URL}/geocode/json?latlng=${lat},${lng}&key=${MAP_KEY}`);
     return { ...data };
   } catch (err) {
     return null;
