5975018e5e654bae5253fa876e0383ce7a5adf41
Carlos Eduardo Flores Ohoa <cefo14@MacBook-Pro-de-Carlos.local>
2019-12-16 11:40:12 -0600

    dist compilatio

--- dist/nodes/SignatureMap/geoCoding.js
+++ dist/nodes/SignatureMap/geoCoding.js
@@ -9,56 +9,44 @@ var _axios = _interopRequireDefault(require("axios"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
-
-function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
 var URL = ' https://maps.googleapis.com/maps/api';
 var MAP_KEY = process.env.MAP_KEY;
 
-var getGeocoding =
-/*#__PURE__*/
-function () {
-  var _ref2 = _asyncToGenerator(
-  /*#__PURE__*/
-  regeneratorRuntime.mark(function _callee(_ref) {
-    var lat, lng, _ref3, data;
+var getGeocoding = function getGeocoding(_ref) {
+  var lat, lng, _ref2, data;
 
-    return regeneratorRuntime.wrap(function _callee$(_context) {
-      while (1) {
-        switch (_context.prev = _context.next) {
-          case 0:
-            lat = _ref.lat, lng = _ref.lng;
-            _context.prev = 1;
-            _context.next = 4;
-            return _axios.default.get("".concat(URL, "/geocode/json?latlng=").concat(lat, ",").concat(lng, "&key=").concat(MAP_KEY));
+  return regeneratorRuntime.async(function getGeocoding$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          lat = _ref.lat, lng = _ref.lng;
+          _context.prev = 1;
+          _context.next = 4;
+          return regeneratorRuntime.awrap(_axios.default.get("".concat(URL, "/geocode/json?latlng=").concat(lat, ",").concat(lng, "&key=").concat(MAP_KEY)));
 
-          case 4:
-            _ref3 = _context.sent;
-            data = _ref3.data;
-            return _context.abrupt("return", _objectSpread({}, data));
+        case 4:
+          _ref2 = _context.sent;
+          data = _ref2.data;
+          return _context.abrupt("return", _objectSpread({}, data));
 
-          case 9:
-            _context.prev = 9;
-            _context.t0 = _context["catch"](1);
-            return _context.abrupt("return", null);
+        case 9:
+          _context.prev = 9;
+          _context.t0 = _context["catch"](1);
+          return _context.abrupt("return", null);
 
-          case 12:
-          case "end":
-            return _context.stop();
-        }
+        case 12:
+        case "end":
+          return _context.stop();
       }
-    }, _callee, null, [[1, 9]]);
-  }));
-
-  return function getGeocoding(_x) {
-    return _ref2.apply(this, arguments);
-  };
-}();
+    }
+  }, null, null, [[1, 9]]);
+};
 
 var _default = getGeocoding;
 exports.default = _default;
