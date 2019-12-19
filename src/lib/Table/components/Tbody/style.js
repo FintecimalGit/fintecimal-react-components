8400dc59b1ad1d80658aa8143267068c228c5f7d
Carlos Eduardo Flores Ohoa <cefo14@MacBook-Pro-de-Carlos.local>
2019-12-19 10:48:44 -0600

    color on hover

--- src/lib/Table/components/Tbody/style.js
+++ src/lib/Table/components/Tbody/style.js
@@ -8,10 +8,12 @@ export default makeStyles((theme) => ({
     '&:hover': {
       boxShadow: '0 24px 38px 3px rgba(0,0,0,0.14), 0 9px 46px 8px rgba(0,0,0,0.12), 0 11px 15px -7px rgba(0,0,0,0.2)',
       cursor: 'pointer',
+      '& > td': {
+        color: theme.palette.primary.main,
+      }
     },
   },
   td: {
-    color: theme.palette.text.primary,
     fontWeight: 'normal',
     padding: theme.spacing(2, 1.5),
     textTransform: 'lowercase',
