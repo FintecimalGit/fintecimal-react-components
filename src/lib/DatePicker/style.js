d91442e68596e5dd90737e33444f60a585cb0471
Alfredo-finte <59579767+Alfredo-finte@users.noreply.github.com>
2020-03-11 19:11:34 -0600

    Correcion de estilo al componente de fecha (#78)
    Co-authored-by: Alfredo Valencia <freddy@Alfredos-MacBook-Pro.local>

--- src/lib/DatePicker/style.js
+++ src/lib/DatePicker/style.js
@@ -1,7 +1,12 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
+  root:{
+    margin: theme.spacing(1),
+  },
   datePicker: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(1),
     width: '100%',
   },
 }));
