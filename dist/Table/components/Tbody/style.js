c0e009d1fe62ab6246b26246e7de4283b472663f
Alfredo Valencia <freddy@Alfredos-MacBook-Pro.local>
2020-10-09 16:44:53 -0500

    Fix sombras en el hover de las filas de una tabla.

--- dist/Table/components/Tbody/style.js
+++ dist/Table/components/Tbody/style.js
@@ -15,7 +15,7 @@ var _default = (0, _styles.makeStyles)(function (theme) {
       borderBottom: 'solid 1px',
       borderBottomColor: (0, _colorManipulator.fade)('#F1F1F1', 0.5),
       '&:hover': {
-        boxShadow: '0 24px 38px 3px rgba(0,0,0,0.14), 0 9px 46px 8px rgba(0,0,0,0.12), 0 11px 15px -7px rgba(0,0,0,0.2)',
+        boxShadow: '0 24px 38px 20px rgba(0,0,0,0.07), 0 23px 46px 20px rgba(0,0,0,0.01), 0 11px 46px 20px rgba(0,0,0,0.07)',
         cursor: 'pointer',
         '& > td': {
           color: theme.palette.primary.main
