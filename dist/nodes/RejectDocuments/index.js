12489e329034f1a530377c29cd7657d75e8ebc4b
Isidro Fernando Gonzalez Gomez <chilito395@gmail.com>
2021-02-11 16:02:14 -0600

    Arreglando multiples imagenes en la vista de documentos para la vista de verificacion.

--- dist/nodes/RejectDocuments/index.js
+++ dist/nodes/RejectDocuments/index.js
@@ -1,7 +1,5 @@
 "use strict";
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
@@ -21,11 +19,11 @@ var _DropZone = _interopRequireDefault(require("../../DropZone"));
 
 var _FilePreview = _interopRequireDefault(require("../../FilePreview"));
 
+var _FileFinder = _interopRequireDefault(require("../../FileFinder"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
 
 function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 
@@ -35,7 +33,7 @@ function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArra
 
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
 
-function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
@@ -57,6 +55,21 @@ var RejectDocuments = function RejectDocuments(_ref) {
       _useState2 = _slicedToArray(_useState, 2),
       file = _useState2[0],
       setFile = _useState2[1];
+
+  var _useState3 = (0, _react.useState)([]),
+      _useState4 = _slicedToArray(_useState3, 2),
+      files = _useState4[0],
+      setFiles = _useState4[1];
+
+  var _useState5 = (0, _react.useState)(0),
+      _useState6 = _slicedToArray(_useState5, 2),
+      currentFile = _useState6[0],
+      setCurrentFile = _useState6[1];
+
+  var _useState7 = (0, _react.useState)(''),
+      _useState8 = _slicedToArray(_useState7, 2),
+      search = _useState8[0],
+      setSearch = _useState8[1];
 
   var generateFileToURL =
   /*#__PURE__*/
@@ -98,13 +111,81 @@ var RejectDocuments = function RejectDocuments(_ref) {
     };
   }();
 
+  var generateFileToURLArray =
+  /*#__PURE__*/
+  function () {
+    var _ref3 = _asyncToGenerator(
+    /*#__PURE__*/
+    regeneratorRuntime.mark(function _callee2() {
+      var i, response, data, metadata, _file;
+
+      return regeneratorRuntime.wrap(function _callee2$(_context2) {
+        while (1) {
+          switch (_context2.prev = _context2.next) {
+            case 0:
+              i = 0;
+
+            case 1:
+              if (!(i < url.length)) {
+                _context2.next = 16;
+                break;
+              }
+
+              _context2.next = 4;
+              return fetch(url[i]);
+
+            case 4:
+              response = _context2.sent;
+              _context2.next = 7;
+              return response.blob();
+
+            case 7:
+              data = _context2.sent;
+              metadata = {
+                type: data.type
+              };
+              _file = new File([data], title, metadata);
+              if (_file) files[i] = _file;
+              console.log(multiple);
+              console.log(files);
+
+            case 13:
+              i++;
+              _context2.next = 1;
+              break;
+
+            case 16:
+              setFile(files[0]);
+
+            case 17:
+            case "end":
+              return _context2.stop();
+          }
+        }
+      }, _callee2);
+    }));
+
+    return function generateFileToURLArray() {
+      return _ref3.apply(this, arguments);
+    };
+  }();
+
   (0, _react.useEffect)(function () {
-    if (url !== '' && typeof url === "string") generateFileToURL();
+    if (url !== '' && typeof url === "string") generateFileToURL();else generateFileToURLArray();
   }, [url]);
 
   var handleOnDrop = function handleOnDrop(value) {
     onHandlerReject(value);
     setFile(value[0]);
+  };
+
+  var handleOnClick = function handleOnClick(index, file) {
+    setFile(file);
+    setCurrentFile(index);
+  };
+
+  var handleOnSearch = function handleOnSearch(text) {
+    setSearch(text);
   };
 
   return _react.default.createElement("div", null, _react.default.createElement("div", {
@@ -141,6 +222,14 @@ var RejectDocuments = function RejectDocuments(_ref) {
     onDrop: handleOnDrop,
     isIncorrect: true,
     multiple: multiple
+  }), multiple && files.length > 0 && _react.default.createElement(_FileFinder.default, {
+    files: files,
+    current: currentFile,
+    onClick: handleOnClick,
+    search: search,
+    onSearch: handleOnSearch,
+    placeholder: 'Buscar',
+    disabled: true
   }));
 };
 
@@ -148,7 +237,7 @@ RejectDocuments.propTypes = {
   title: _propTypes.default.string.isRequired,
   onDrop: _propTypes.default.func.isRequired,
   onReject: _propTypes.default.func,
-  url: _propTypes.default.string,
+  url: _propTypes.default.any,
   rejectionOptions: _propTypes.default.array,
   rejectionData: _propTypes.default.object,
   onHandlerReject: _propTypes.default.func.isRequired,
@@ -161,7 +250,7 @@ RejectDocuments.defaultProps = {
   title: '',
   onDrop: function onDrop() {},
   onReject: function onReject() {},
-  url: '',
+  url: [],
   rejectionOptions: [],
   rejectionData: {
     reason: '',
@@ -170,7 +259,7 @@ RejectDocuments.defaultProps = {
   showUndo: false,
   onUndoRejection: function onUndoRejection() {},
   editable: true,
-  multiple: false
+  multiple: true
 };
 var _default = RejectDocuments;
 exports.default = _default;
